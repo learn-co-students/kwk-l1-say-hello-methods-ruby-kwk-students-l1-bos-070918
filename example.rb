e7b46045c5be2ecb8943a59109c131af107e4dd7
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-10 15:55:45 +0000

    Done.

---
+++ example.rb
@@ -0,0 +1,8 @@
+
+def say_hello(your_name)
+  puts "Hello, #{your_name}!"
+end
+
+say_hello("Pelumi")
+say_hello("Brandon")
+say_hello("Devan")
--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,4 @@
-# Build your say_hello method here
+def say hello(name = "Ruby Progammer")
+  puts "Hello #{name}!"
+end
+  # Build your say_hello method here
